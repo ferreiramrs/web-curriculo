3202ad5e7ad364b3f11cb2a6916aee4be5093eda
Mariana <marihstonej@gmail.com>
2026-06-29 00:14:00 -0300

    conslusão do site

--- src/components/script.js
+++ src/components/script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Erro:', error));
 
-        //2. bloco do Rodapé 
+        //2. Bloco do Footer/Rodapé
         fetch('../components/footer.html')
             .then(res => res.text())
             .then(data => {
@@ -28,3 +28,17 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .catch(error => console.error('Erro ao carregar o footer:', error));
         }); //Esta chave fecha o DOMContentLoaded corretamente
+
+//script para o formulário seguir para destino.html
+document.getElementById('formulário').addEventListener('submit',function(event){
+    const nome = document.getElementById('nome').ariaValueMax;
+
+    //Validação Simples
+    if(nome.length < 3){
+        alert("Por favor, insira um nome com pelo menos 3 caracteres.");
+        event.preventDefault(); //Impede o envio se a condição não for atendida
+        return;
+    }
+    //Se tudo estiver ok, o navegador prosseguirá naturalmente para destino.html
+    console.log("Formulário enviado com sucesso!")
+});
